aceb8585c20f2b52fc0b9c8b536b68237661cf93
Luiz-frj <luizfrj200412@gmail.com>
2025-04-14 11:04:06 -0300

    Criação dos feeds

--- app/src/main/java/br/edu/ifsp/dmo/feitopormim/HomeActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/feitopormim/HomeActivity.kt
@@ -3,33 +3,24 @@ package br.edu.ifsp.dmo.feitopormim
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.edu.ifsp.dmo.feitopormim.adapter.PostAdapter
 import br.edu.ifsp.dmo.feitopormim.databinding.ActivityHomeBinding
+import br.edu.ifsp.dmo.feitopormim.model.Post
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 
 class HomeActivity: AppCompatActivity() {
     private lateinit var binding:ActivityHomeBinding
+    private lateinit var adapter: PostAdapter;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val firebaseAuth = FirebaseAuth.getInstance()
-        val db = Firebase.firestore
-        val email = firebaseAuth.currentUser!!.email.toString()
-        db.collection("usuarios").document(email).get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful){
-                    val document = task.result
-                    val imageString = document.data!!["fotoPerfil"].toString()
-                    val bitmap = Base64Converter.stringToBitmap(imageString)
-                    binding.profileImage.setImageBitmap(bitmap)
-                    binding.userName.text = document.data!!["username"].toString()
-                    binding.completeName.text =
-                        document.data!!["nomeCompleto"].toString()
-                }
-            }
+
 
         binding.buttonExit.setOnClickListener {
             firebaseAuth.signOut();
@@ -37,9 +28,24 @@ class HomeActivity: AppCompatActivity() {
             finish();
         }
 
-        binding.goPost.setOnClickListener {
-            startActivity(Intent(this,PostActivity::class.java))
-            finish();
+        binding.goFeed.setOnClickListener {
+            val db = Firebase.firestore
+            db.collection("posts").get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        val document = task.result
+                        val posts = ArrayList<Post>()
+                        for (document in document.documents) {
+                            val imageString = document.data!!["imageString"].toString()
+                            val bitmap = Base64Converter.stringToBitmap(imageString)
+                            val descricao = document.data!!["descricao"].toString()
+                            posts.add(Post(descricao, bitmap))
+                        }
+                        adapter = PostAdapter(posts.toTypedArray())
+                        binding.recycleView.layoutManager = LinearLayoutManager(this)
+                        binding.recycleView.adapter = adapter
+                    }
+                }
         }
     }
 }
---
+++ app/src/main/java/br/edu/ifsp/dmo/feitopormim/adapter/PostAdapter.kt
@@ -0,0 +1,31 @@
+package br.edu.ifsp.dmo.feitopormim.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.ifsp.dmo.feitopormim.R
+import br.edu.ifsp.dmo.feitopormim.model.Post
+
+class PostAdapter(private val posts: Array<Post>) :
+    RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    {
+        val imgPost : ImageView = view.findViewById(R.id.imageCard)
+        val txtDescricao : TextView = view.findViewById(R.id.descricaoPost)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.post_item, parent, false)
+        return ViewHolder(view)
+    }
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.txtDescricao.text = posts[position].getDescricao()
+        holder.imgPost.setImageBitmap(posts[position].getFoto())
+    }
+}
